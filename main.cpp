84404a5ce85a9fda84126d2346aec964497598a2
Justin Cirigliano <cirigj@rpi.edu>
2015-05-09 16:19:09 -0400

    Main socket-forking

---
+++ main.cpp
@@ -0,0 +1,81 @@
+/*Main*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <netdb.h>
+
+#define BUFFER_SIZE 1024
+
+using namespace std;
+
+int main() {
+	//Create a listener socket
+	int sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		cerr << "Failed to create a listener socket. Exiting..." << endl;
+		exit(EXIT_FAILURE);
+	}
+	//Socket address struct
+	struct sockaddr_in server;
+	server.sin_family = PF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	//Assign port
+	unsigned short port = 8765;
+	server.sin_port = htons(port);
+	//Bind socket to address
+	int serverSize = sizeof(server);
+	if (bind(sock, (struct sockaddr *)&server, serverSize) < 0) {
+		cerr << "Failed to bind socket. Exiting..." << endl;
+		exit(EXIT_FAILURE);
+	}
+	//Listen for up to SOMAXCONN clients
+	listen(sock, 128);
+	cout << "Listening on port " << port << endl;
+
+	struct sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	int pid;
+	char buffer[BUFFER_SIZE];
+
+	while (1) {
+		int newsock = accept(sock, (struct sockaddr *)&client, (socklen_t*)&clientSize);
+		//Get hostname
+		char hostName[NI_MAXHOST];
+		struct in_addr ipv4addr;
+		inet_pton(AF_INET, inet_ntoa(client.sin_addr), &ipv4addr);
+		if (getnameinfo(client, clientSize, hostName, sizeof(hostName), NULL, 0, NI_NAMEREQD) != 0) {
+			cout << "Received incoming connection from unresolved host" << endl;
+		}
+		else {
+			cout << "Received incoming connection from " << hostName << endl;
+		}
+		//Fork the new socket
+		pid = fork();
+		//Client fork doesn't exist
+		if (pid < 0) {
+			cerr << "Could not fork client socket. Exiting..." << endl;
+			exit(EXIT_FAILURE);
+		}
+		//Client thread
+		else if (pid == 0) {
+			int n;
+			do {
+				n = 0; //***TEMP***
+			} while (n > 0);
+			cout << "Client closed its socket....terminating" << endl;
+		}
+		//Server
+		else {
+			close(newsock);
+		}
+	}
+	close(sock);
+	return EXIT_SUCCESS;
+}
